fc84ee5608b9a17575b028828fc577365afb6aab
Tim Keeley <tkeeley@hotmail.com>
2018-11-07 21:18:25 -0500

    Delete Phone_num_handling.js

--- Phone_num_handling.js
+++
@@ -1,4 +0,0 @@
-var phone = prompt("Please enter your phone number");
-// ************Phone Number Input Handling**************
-var phoneRaw = phone.replace(/\D/g,'');
-var phoneNumber = phoneRaw.substr(-10);
